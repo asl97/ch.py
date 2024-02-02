01dcaae327cb820d20d5c6476c17d3eb0b7a6d7e
ASL97 <asl97@airmail.cc>
2024-02-03 01:13:21 +0800

    Make it so error in the thread doesn't hang pytest
    We can now directly assert in the replacement/checking function

--- ch/tests/test_user.py
+++ ch/tests/test_user.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-from typing import Callable, Optional
+from typing import Any, Callable, Optional
 import threading
 import asyncio
 import pytest
@@ -13,23 +13,51 @@ class Bot(ch.mixin.WindowsMainLoopFix, ch.RoomManager):
     # using threading for non deterministic modification of tasks or conns (rooms)
     disconnectOnEmptyConnAndTask = False
 
-
 class CallEvent():
     def __init__(self, instance: ch.RoomManager , replacement: Optional[dict[str, Callable[..., None]]] = None):
         self.instance = instance
         self.func = instance._callEvent
         instance._callEvent = self.call
         self.replacement: dict[str, Callable[..., None]] = replacement or dict()
+        self.future: asyncio.Future[Any]
+        self.fatal_error = False
+
+    def run_instance(self):
+        try:
+            self.instance.main()
+        except Exception as e:
+            self.fatal_error = True
+            self.setFutureResult(e)
+
+    def setFutureResult(self, item: Any):
+        self.future.get_loop().call_soon_threadsafe(self.future.set_result, item)
+
+    def resetFuture(self):
+        loop = asyncio.get_event_loop()
+        self.future = loop.create_future()
+
+    async def checkSuccess(self):
+        result = await self.future
+        if issubclass(type(result), Exception):
+            raise result
+
+        return result
 
     def setReplacement(self, replacement: dict[str, Callable[..., None]] = None):
         self.replacement = replacement
+        self.resetFuture()
 
     def clearReplacement(self):
         self.replacement.clear()
 
     def call(self, conn: ch.Conn, evt: str, *args: ..., **kw: ...):
         if (func := self.replacement.get(evt)) is not None:
-            func(self.instance, conn, *args, **kw)
+            try:
+                func(self.instance, conn, *args, **kw)
+            except Exception as e:
+                self.setFutureResult(e)
+            else:
+                self.setFutureResult(None)
         else:
             self.func(conn, evt, *args, **kw)
 
@@ -45,41 +73,36 @@ class TestCases():
         if cls.config['username'] and cls.config['password']:
             cls.instance = Bot(cls.config['username'], cls.config['password'])
             cls.CE = CallEvent(cls.instance)
-            cls.instance_thread = threading.Thread(target=cls.instance.main, name='bot', daemon=True)
+            cls.instance_thread = threading.Thread(target=cls.CE.run_instance, name='bot', daemon=True)
             cls.instance_thread.start()
         else:
             raise EnvironmentError('Password not found in ENV')
+
+    def setup_method(self, method):
+        if self.CE.fatal_error:
+            pytest.exit('fatal error encountered, bot thread stopped')
 
     def teardown_method(self, method):
         self.CE.clearReplacement()
 
     @pytest.mark.asyncio
     async def test_join_and_connect(tst):
-        loop = asyncio.get_event_loop()
-        future = loop.create_future()
-
         def onConnect(self, room):
-            future.get_loop().call_soon_threadsafe(future.set_result, room.name == tst.config['roomname'])
+            assert room.name == tst.config['roomname']
 
         tst.CE.setReplacement({'onConnect': onConnect})
         tst.instance.joinRoom(tst.config['roomname'])
  
-        assert await future
+        await tst.CE.checkSuccess()
 
     @pytest.mark.asyncio
     async def test_message_and_receive(tst):
-        loop = asyncio.get_event_loop()
-        future = loop.create_future()
-
         msg = "testing sending and receiving message"
 
         def onMessage(self, room, user, message):
-            results = []
-            results.append(self.user == user == message.user)
-            results.append(message.body == msg)
-            results.append(room.name == tst.config['roomname'] == message.room.name)
-
-            future.get_loop().call_soon_threadsafe(future.set_result, results)
+            assert self.user == user == message.user
+            assert message.body == msg
+            assert room.name == tst.config['roomname'] == message.room.name
 
         tst.CE.setReplacement({'onMessage': onMessage})
         if room := tst.instance.getRoom(tst.config['roomname']):
@@ -87,7 +110,7 @@ class TestCases():
         else:
             raise RuntimeError('Room not found')
  
-        assert all(await future)
+        await tst.CE.checkSuccess()
 
     @classmethod
     def teardown_class(cls):
